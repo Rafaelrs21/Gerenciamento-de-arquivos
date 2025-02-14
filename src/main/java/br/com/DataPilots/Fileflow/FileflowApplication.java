12b5d0e6d3895d6b2785be35020e344a22f2fb9f
Rafaelrs21 <santireis14@gmail.com>
2025-02-14 17:30:23 -0300

    first commit

---
+++ src/main/java/br/com/DataPilots/Fileflow/FileflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.DataPilots.Fileflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/DataPilots/Fileflow/FileflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.DataPilots.Fileflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
