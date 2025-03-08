b27ba7d8687821f77d5733fd8edb6b39c088529b
Miguel Wiest <wiestmiguel@gmail.com>
2025-03-08 13:48:41 -0300

    feature: create folder arquives

---
+++ src/main/java/br/com/DataPilots/Fileflow/services/FolderService.java
@@ -0,0 +1,18 @@
+package br.com.DataPilots.Fileflow.services;
+
+import br.com.DataPilots.Fileflow.AppConsts;
+import br.com.DataPilots.Fileflow.entities.User;
+import br.com.DataPilots.Fileflow.exceptions.InvalidPasswordLengthException;
+import br.com.DataPilots.Fileflow.exceptions.InvalidUserException;
+import br.com.DataPilots.Fileflow.exceptions.UserAlreadyExistsException;
+import br.com.DataPilots.Fileflow.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FolderService {
+    @Autowired
+    private FolderRepository repository;
+    
+}
