4848a8caf450fa3e00e579f01d7174854db8bada
taohansens <taohansens@gmail.com>
2025-03-09 22:49:13 -0300

    [FIX] Ajustes na classe Folder.

--- src/main/java/br/com/DataPilots/Fileflow/entities/Folder.java
+++ src/main/java/br/com/DataPilots/Fileflow/entities/Folder.java
@@ -8,17 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Table(name="folders")
 @Entity(name="Folder")
-@Getter
-@NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of="id")
+@NoArgsConstructor
+@Data
 public class Folder {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
