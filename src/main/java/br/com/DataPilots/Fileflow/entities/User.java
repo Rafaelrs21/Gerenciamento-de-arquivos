35f05a0347ae6932403ca14375a0cb16e36624c1
Rafaelrs21 <santireis14@gmail.com>
2025-03-11 18:04:22 -0300

    Adicionado dialetic aos bancos e lombok atualizado

--- src/main/java/br/com/DataPilots/Fileflow/entities/User.java
+++ src/main/java/br/com/DataPilots/Fileflow/entities/User.java
@@ -3,7 +3,6 @@ package br.com.DataPilots.Fileflow.entities;
 import java.util.Collection;
 import java.util.List;
 
-import br.com.DataPilots.Fileflow.dtos.CreateUserRequestDTO;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
