d59d0c92f72647546acbf4840f3ddf13cae3d767
Rafaelrs21 <santireis14@gmail.com>
2025-03-25 16:54:35 -0300

    [REFACT] Feito refact nas classes

--- src/main/java/br/com/DataPilots/Fileflow/controllers/FileController.java
+++ src/main/java/br/com/DataPilots/Fileflow/controllers/FileController.java
@@ -3,16 +3,7 @@ package br.com.DataPilots.Fileflow.controllers;
 import java.util.List;
 import java.util.Map;
 
-import br.com.DataPilots.Fileflow.dtos.DefaultResponseDTO;
-import br.com.DataPilots.Fileflow.entities.User;
-import br.com.DataPilots.Fileflow.services.FileService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,12 +16,10 @@ import br.com.DataPilots.Fileflow.repositories.FileRepository;
 @RequiredArgsConstructor
 public class FileController {
     private final FileRepository fileRepository;
-    private final FileService fileService;
 
     @GetMapping
-    public ResponseEntity<List<File>> getAllFile(@AuthenticationPrincipal User user) {
-        List<File> all = fileRepository.findAll();
-        ObjectMapper objectMapper = new ObjectMapper();
-        return new ResponseEntity<>(all, HttpStatus.OK);
+    public List<Map<String, Object>> getAll() {
+        List<File> files = fileRepository.findAll();
+        return files.stream().map(File::serialize).toList();
     }
 }
