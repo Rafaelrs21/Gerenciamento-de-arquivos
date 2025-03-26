2b04fcd42bdfb747facdf9a8335093109ad36c5b
taohansens <taohansens@gmail.com>
2025-03-26 15:53:35 -0300

    [PR80] Adição da anotação @Repository

--- src/main/java/br/com/DataPilots/Fileflow/repositories/FileRepository.java
+++ src/main/java/br/com/DataPilots/Fileflow/repositories/FileRepository.java
@@ -2,7 +2,9 @@ package br.com.DataPilots.Fileflow.repositories;
 
 import br.com.DataPilots.Fileflow.entities.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FileRepository extends JpaRepository<File, Long> {
     
 }
--- src/main/java/br/com/DataPilots/Fileflow/repositories/UserRepository.java
+++ src/main/java/br/com/DataPilots/Fileflow/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package br.com.DataPilots.Fileflow.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.DataPilots.Fileflow.entities.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public Optional<User> findByUsername(String username);
