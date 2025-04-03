7041f826dae9250126b5d7e2eb4494c2eb66b0d4
taohansens <taohansens@gmail.com>
2025-04-02 21:39:32 -0300

    [FIX] Anotação @Repository

--- src/main/java/br/com/DataPilots/Fileflow/repositories/FolderRepository.java
+++ src/main/java/br/com/DataPilots/Fileflow/repositories/FolderRepository.java
@@ -2,7 +2,9 @@ package br.com.DataPilots.Fileflow.repositories;
 
 import br.com.DataPilots.Fileflow.entities.Folder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
 
 }
