4946f2fc1db4346d8f8a821df87131a4386e2fb8
taohansens <taohansens@gmail.com>
2025-04-02 22:04:29 -0300

    [FIX] Modificadores de acessos redundantes.

--- src/main/java/br/com/DataPilots/Fileflow/repositories/UserRepository.java
+++ src/main/java/br/com/DataPilots/Fileflow/repositories/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    public boolean existsByUsername(String username);
+    boolean existsByUsername(String username);
 }
