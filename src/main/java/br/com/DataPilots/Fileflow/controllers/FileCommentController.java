4511e3280b81f3c76ef59f69f71a9a3890e422d8
taohansens <taohansens@gmail.com>
2025-04-02 21:44:50 -0300

    [FEAT] Controller/Service/Repository

---
+++ src/main/java/br/com/DataPilots/Fileflow/controllers/FileCommentController.java
@@ -0,0 +1,47 @@
+package br.com.DataPilots.Fileflow.controllers;
+
+import br.com.DataPilots.Fileflow.entities.FileComment;
+import br.com.DataPilots.Fileflow.services.FileCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/file-comments")
+@RequiredArgsConstructor
+public class FileCommentController {
+    private final FileCommentService fileCommentService;
+
+    @GetMapping("/file/{fileId}")
+    public ResponseEntity<List<FileComment>> getAllCommentsByFile(@PathVariable Long fileId) {
+        List<FileComment> comments = fileCommentService.findAllByFileId(fileId);
+        return ResponseEntity.ok(comments);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<FileComment>> getAllCommentsByUser(@PathVariable Long userId) {
+        List<FileComment> comments = fileCommentService.findAllByUserId(userId);
+        return ResponseEntity.ok(comments);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FileComment> getCommentById(@PathVariable Long id) {
+        Optional<FileComment> comment = fileCommentService.findById(id);
+        return comment.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<FileComment> createComment(@RequestBody FileComment fileComment) {
+        FileComment savedComment = fileCommentService.save(fileComment);
+        return ResponseEntity.ok(savedComment);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCommentById(@PathVariable Long id) {
+        fileCommentService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
