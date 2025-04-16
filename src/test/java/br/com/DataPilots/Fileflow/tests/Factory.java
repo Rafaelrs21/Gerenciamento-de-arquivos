116680cff4b5bef6ce85bcee9a2c7c3bb99167d6
Lucio Rubens <lucio.caetano@al.infnet.edu.br>
2025-04-16 06:58:40 -0300

    [Feature] - Controle de permissão para usuários

--- src/test/java/br/com/DataPilots/Fileflow/tests/Factory.java
+++ src/test/java/br/com/DataPilots/Fileflow/tests/Factory.java
@@ -1,11 +1,19 @@
 package br.com.DataPilots.Fileflow.tests;
 
+import br.com.DataPilots.Fileflow.entities.File;
 import br.com.DataPilots.Fileflow.entities.Folder;
 import br.com.DataPilots.Fileflow.entities.User;
 
 public class Factory {
     public static User createUser(String username, String password) {
         return new User(username, password);
+    }
+
+    public static File createFile() {
+        File file = new File();
+        file.setName("File");
+        file.setId(1L);
+        return file;
     }
 
     public static Folder createFolder() {
