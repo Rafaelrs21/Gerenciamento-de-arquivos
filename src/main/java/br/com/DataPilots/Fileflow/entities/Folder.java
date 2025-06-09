870f53f14d5da86044326b251f61ae9c579f1b0d
Lucio Rubens <lucio.caetano@al.infnet.edu.br>
2025-06-09 16:41:49 +0100

    [Feat] Implementar cache com Redis

--- src/main/java/br/com/DataPilots/Fileflow/entities/Folder.java
+++ src/main/java/br/com/DataPilots/Fileflow/entities/Folder.java
@@ -1,5 +1,6 @@
 package br.com.DataPilots.Fileflow.entities;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,7 +14,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Folder {
+public class Folder implements Serializable {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private Long userId;
